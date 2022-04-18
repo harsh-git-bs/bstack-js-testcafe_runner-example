29b364eb7610c1e3a27ad2dbc1fbdf1c10618738
harsh-git-bs <harsh.j@browserstack.com>
2022-04-18 16:20:08 +0530

    runner update

--- runner.bat
+++ runner.bat
@@ -1,6 +1,11 @@
 @ECHO OFF
 set BROWSERSTACK_USE_AUTOMATE=1
 set BROWSERSTACK_DEBUG=true
+set BROWSERSTACK_VIDEO="true"
+
+set BROWSERSTACK_BUILD_ID="Nintendo"
+set BROWSERSTACK_PROJECT_NAME="Nintendo"
+set BROWSERSTACK_TEST_RUN_NAME="Nintendo"
 
 if %1%==single ( 
     set BROWSERSTACK_TEST_RUN_NAME= Browserstack Single Search & echo "Running Single testcafe test on Browserstack" & node .\testcafe_runner_single.js 
