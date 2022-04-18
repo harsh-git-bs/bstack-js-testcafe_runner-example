13dcce48f54ff6d521fdedfdefd00062fa81ff31
harsh-git-bs <harsh.j@browserstack.com>
2022-04-18 16:19:03 +0530

    beta browser added

--- testcafe_runner_parallel.js
+++ testcafe_runner_parallel.js
@@ -2,10 +2,15 @@ import createTestCafe from "testcafe";
 
 var sources = ["index.js"];
 var browsers = [
-  "browserstack:iPhone 12 Mini",
-  "browserstack:Samsung Galaxy S20",
-  "browserstack:safari:OS X Big Sur",
-  "browserstack:chrome:Windows 10",
+  // "browserstack:iPhone 12 Mini",
+  // "browserstack:Samsung Galaxy S20",
+  // "browserstack:safari:OS X Big Sur",
+  // "browserstack:chrome-1:Windows 10",
+  "browserstack:chrome@101.0 beta:Windows 11",
+  "browserstack:firefox@100.0 beta:Windows 11",
+  "browserstack:edge@100.0 beta:Windows 11",
+  "browserstack:opera@12.16:Windows 8.1",
+  "browserstack:ie@11.0:Windows 10",
 ];
 
 async function testrunner() {
